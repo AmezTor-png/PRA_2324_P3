413a311bed11f0b23668c1561ad62ae2ed7317c7
Iván Tortajada Amézcua <itorame@upv.edu.es>
2025-12-16 11:47:34 +0100

    Fix output formatting in TableEntry friend function

--- TableEntry.h
+++ TableEntry.h
@@ -36,7 +36,8 @@ class TableEntry
 
     friend std::ostream& operator << (std::ostream &out, const TableEntry<V> &te)
     {
-      
+      out << te.key << " -> " << te.value;
+      return out;
     }
 };
 
